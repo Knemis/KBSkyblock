807d43c600c3bac0659c25982a05ed77de5b5aaf
knemis <alc13no@gmail.com>
2025-06-04 17:40:40 +0300

    buildable

--- src/main/java/com/kbskyblock/KBSkyblock/KBSkyblock.java
+++ src/main/java/com/kbskyblock/KBSkyblock/KBSkyblock.java
@@ -2,16 +2,19 @@ package com.kbskyblock.KBSkyblock;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.logging.Level;
+
 public final class KBSkyblock extends JavaPlugin {
 
     @Override
     public void onEnable() {
+        getLogger().log(Level.INFO, "KBSkyblock has been enabled!");
         // Plugin startup logic
-
     }
 
     @Override
     public void onDisable() {
+        getLogger().log(Level.INFO, "KBSkyblock has been disabled!");
         // Plugin shutdown logic
     }
 }
