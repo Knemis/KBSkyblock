d70383efcdee26176e4ef22edcfd011c584fafdf
knemis <alc13no@gmail.com>
2025-06-04 14:25:22 +0300

    adjustments

--- IridiumCore-master/multiversion/common/src/main/java/com/iridium/iridiumcore/multiversion/IridiumInventory.java
+++
@@ -1,8 +0,0 @@
-package com.iridium.iridiumcore.multiversion;
-
-import org.bukkit.entity.Player;
-import org.bukkit.inventory.Inventory;
-
-public abstract class IridiumInventory {
-    public abstract Inventory getTopInventory(Player player);
-}
--- IridiumCore-master/plugin/src/main/java/com/iridium/iridiumcore/JavaPluginSupplier.java
+++
@@ -1,7 +0,0 @@
-package com.iridium.iridiumcore;
-
-import org.bukkit.plugin.java.JavaPlugin;
-
-public interface JavaPluginSupplier<T> {
-    T get(JavaPlugin javaPlugin);
-}
