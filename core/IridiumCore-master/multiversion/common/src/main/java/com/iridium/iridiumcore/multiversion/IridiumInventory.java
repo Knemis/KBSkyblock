75f70a4a83ef013d74f42140a7d166591842eacc
knemis <alc13no@gmail.com>
2025-06-05 16:34:56 +0300

    lots of updates for configuration

--- core/IridiumCore-master/multiversion/common/src/main/java/com/iridium/iridiumcore/multiversion/IridiumInventory.java
+++
@@ -1,8 +0,0 @@
-package com.kbskyblock.core.multiversion;
-
-import org.bukkit.entity.Player;
-import org.bukkit.inventory.Inventory;
-
-public abstract class CoreInventory {
-    public abstract Inventory getTopInventory(Player player);
-}
--- core/IridiumCore-master/plugin/src/main/java/com/iridium/iridiumcore/JavaPluginSupplier.java
+++
@@ -1,7 +0,0 @@
-package com.kbskyblock.core;
-
-import org.bukkit.plugin.java.JavaPlugin;
-
-public interface JavaPluginSupplier<T> {
-    T get(JavaPlugin javaPlugin);
-}
